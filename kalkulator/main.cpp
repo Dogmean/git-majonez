1e283503ef07498464448eedbca0bc739d18d3b1
ExiDK <exideek@gmail.com>
2019-01-11 21:35:20 +0100

    Created basic main function for kalkulator.

--- kalkulator/main.cpp
+++ kalkulator/main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+int a, b;
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+	int operacja;
+	cout<<"Podaj liczbe a: ";cin>>a;cout<<endl;
+	cout<<"Podaj liczbe b: ";cin>>b;cout<<endl;
+	cout<<"Co chcesz zrobic?"<<endl;
+	cout<<"Dodac a do b [1]"<<endl<<"Odjac a od b [2]"<<endl<<"Pomnozyc a przez b [3]"<<endl<<"Podzielic a przez b [4]"<<endl;
+	cin>>operacja;
+
+	if(operacja == "1")
+		dodawanie(a,b);
+	else if(operacja == "2")
+		odejmowanie(a,b);
+	else if(operacja == "3")
+		mnozenie(a,b);
+	else if(operacja == "4")
+		dzielenie(a,b);
+	else cout <<"Zle wybrales, z nami koniec. Zegnaj.";
+	return 0;
 }

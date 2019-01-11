719d185959fc70abef614a97261608618d689027
n0wik <pawel1998no@o2.pl>
2019-01-11 23:27:34 +0100

    zeby ladnie wygladalo

--- kalkulator/main.cpp
+++ kalkulator/main.cpp
@@ -9,8 +9,12 @@ int a, b;
 int main()
 {
 	int operacja;
-	cout<<"Podaj liczbe a: ";cin>>a;cout<<endl;
-	cout<<"Podaj liczbe b: ";cin>>b;cout<<endl;
+	cout<<"Podaj liczbe a: ";
+	cin>>a;
+	cout<<endl;
+	cout<<"Podaj liczbe b: ";
+	cin>>b;
+	cout<<endl;
 	cout<<"Co chcesz zrobic?"<<endl;
 	cout<<"Dodac a do b [1]"<<endl<<"Odjac a od b [2]"<<endl<<"Pomnozyc a przez b [3]"<<endl<<"Podzielic a przez b [4]"<<endl;
 	cin>>operacja;
